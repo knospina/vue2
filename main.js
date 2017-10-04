6f90285aff32ed20c25e11f71e25d89e7a449b67
Evita Knospina <evita.knospina@mindlytix.com>
2017-10-04 14:03:27 +0200

    Recent

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+window.Event = new class {
+
+    constructor() {
+
+        this.vue = new Vue();
+
+    }
+
+    fire(event, data = null) {
+
+        this.vue.$emit(event, data);
+
+    }
+
+    listen(event, callback) {
+
+        this.vue.$on(event, callback);
+
+    }
+
+}
+
 Vue.component('task-list', {
 
     'template': `
@@ -76,18 +98,124 @@ Vue.component('message', {
 Vue.component('modal', {
 
     'template': `
-    <div class="modal is-active">
-        <div class="modal-background"></div>
-        <div class="modal-content">
-            <div class="box">
-                <slot></slot>
-            </div>
-        </div>
-        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    <div class="moda is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Modal title</p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          Temporary content goes here
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Save changes</button>
+          <button class="button">Cancel</button>
+        </footer>
+      </div>
     </div>
     `
 
 });
+
+Vue.component('tabs', {
+
+    'template': `
+        <nav>
+            <div class="tabs">
+                <ul>
+                    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                        <a :href="tab.href" @click="selectTab(tab)">
+                            {{tab.name}}
+                        </a>
+
+                    </li>
+                </ul>
+            </div>
+
+            <div class="tabs-details">
+                <slot></slot>
+            </div>
+        </nav>
+    `,
+
+    data() {
+        return {
+            'tabs': []
+        }
+    },
+
+
+    created() {
+
+        this.tabs = this.$children;
+
+    },
+
+    'methods': {
+        selectTab(selectedTab) {
+
+            this.tabs.forEach(tab => {
+
+                tab.isActive = (tab.name == selectedTab.name);
+
+            });
+
+        }
+    }
+
+});
+
+Vue.component('tab', {
+
+    'template': `
+        <div v-show="isActive">
+            <slot></slot>
+        </div>
+    `,
+
+    'props': {
+        'name': {
+            'required': true
+        },
+        'selected': {
+            'default': false
+        }
+    },
+
+    data() {
+        return {
+            'isActive': false
+        };
+    },
+
+    'computed': {
+
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+
+    },
+
+    mounted() {
+        this.isActive = this.selected;
+    }
+
+
+});
+
+Vue.component('coupon', {
+    'template': `
+        <input placeholder="Enter your coupon code" @blur="onCouponApplied">
+
+    `,
+
+    methods: {
+        onCouponApplied() {
+            Event.fire('applied');
+        }
+    }
+})
 
 new Vue({
 
@@ -95,7 +223,12 @@ new Vue({
 
     'data': {
         'name': 'This is general',
-        'showModal': false
+        'showModal': false,
+        'couponApplied': false
+    },
+
+    created() {
+        Event.listen('applied', () => alert('Handling it'))
     }
 
 });
